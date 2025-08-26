10dba79d2ad5780875876c4cde9661e57cc0146a
Johnny Tseng <johnnie.tseng@gmail.com>
2025-08-26 22:12:38 +0800

    Initial commit

--- SafariNflxAutoSkip Extension/Resources/content.js
+++ SafariNflxAutoSkip Extension/Resources/content.js
@@ -1,7 +1,39 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+const skipButtons = () => {
+  const targets = [
+    {
+      selector: 'button[data-uia="player-skip-intro"]',
+      message: '已略過開場！🍿'
+    },
+    {
+      selector: 'button[data-uia="player-skip-recap"]',
+      message: '已略過前情提要！🕵️‍♂️'
+    }
+  ];
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+  targets.forEach(({ selector, message }) => {
+    const btn = document.querySelector(selector);
+    if (btn) {
+      btn.click();
+      console.log(`[Netflix Skipper] ⏩ Clicked: ${selector}`);
+      showToast(message);
+    }
+  });
+};
+
+setInterval(skipButtons, 1000);
+
+const observer = new MutationObserver(skipButtons);
+observer.observe(document.body, { childList: true, subtree: true });
+
+function showToast(text, type) {
+    const backgroundColor = "#36B361"
+    const playerContainer = document.querySelector('.watch-video')
+    Toastify({
+        text: text,
+        selector: playerContainer,
+        position: "center",
+        style: {
+            background: backgroundColor
+        },
+    }).showToast()
+}
